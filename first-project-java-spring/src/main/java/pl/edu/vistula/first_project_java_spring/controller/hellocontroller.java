262565bfe06aca824a4b9f520523b4bfcb4300f0
Akshay pottekattpremkumar <akshayinfinity2020@gmail.com>
2025-05-22 18:30:31 +0200

    Update hellocontroller.java

--- first-project-java-spring/src/main/java/pl/edu/vistula/first_project_java_spring/controller/hellocontroller.java
+++ first-project-java-spring/src/main/java/pl/edu/vistula/first_project_java_spring/controller/hellocontroller.java
@@ -16,12 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
         public String hello() { return "Hello vistula, in my first spring controller";}
 
 
-        @GetMapping( "/greeting")
-        public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-            model.addAttribute("name", "vistula");
-            return "greeting";
+     @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
         }
-     //http://localhost:8080/greeting?name=Vistula
+     //http://localhost:8080/greeting?name=akssyyy
     }
 
 
