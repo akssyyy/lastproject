93a3c86ac4df714994b5925750a84dcbeddd9a20
Akshay pottekattpremkumar <akshayinfinity2020@gmail.com>
2025-05-22 18:31:05 +0200

    Update hellocontroller.java

--- first-project-java-spring/src/main/java/pl/edu/vistula/first_project_java_spring/controller/hellocontroller.java
+++ first-project-java-spring/src/main/java/pl/edu/vistula/first_project_java_spring/controller/hellocontroller.java
@@ -1,5 +1,6 @@
 package pl.edu.vistula.first_project_java_spring.controller;
 
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
